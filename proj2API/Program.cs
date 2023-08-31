390204789ac647b3da6933aeb23b8bafb06e66e7
Refilwe Mmutlwane <refilwemmutlwane@gmail.com>
2023-08-31 13:56:51 +0200

    Added code that enables me to run the API from Azure

--- proj2API/Program.cs
+++ proj2API/Program.cs
@@ -110,6 +110,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication16");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.UseHttpsRedirection();
 
